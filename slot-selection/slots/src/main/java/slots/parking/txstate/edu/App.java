d1fc3190cfc8ab425c93d5f281eb357e653e56db
mehak <mehakagarwal21@yahoo.com>
2018-02-25 13:35:37 -0600

    slots setup

---
+++ slot-selection/slots/src/main/java/slots/parking/txstate/edu/App.java
@@ -0,0 +1,13 @@
+package slots.parking.txstate.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
